a4691d19b7924f32fd6ad3c70ead429270a32b36
Miti Mazmudar <mitimazmudar@gmail.com>
2023-09-18 16:36:17 -0400

    feat: sample envoy filter

---
+++ privacy-profile-composer/envoy-filter/envoy-filter.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"github.com/envoyproxy/envoy/contrib/golang/common/go/api"
+	"strconv"
+)
+
+var UpdateUpstreamBody = "upstream response body updated by the simple plugin"
+
+type filter struct {
+	api.PassThroughStreamFilter
+
+	callbacks api.FilterCallbackHandler
+	path      string
+	config    *config
+}
+
+func (f *filter) sendLocalReplyInternal() api.StatusType {
+	body := fmt.Sprintf("%s, path: %s\r\n", f.config.echoBody, f.path)
+	f.callbacks.SendLocalReply(200, body, nil, 0, "")
+	return api.LocalReply
+}
+
+// Callbacks which are called in request path
+func (f *filter) DecodeHeaders(header api.RequestHeaderMap, endStream bool) api.StatusType {
+	f.path, _ = header.Get(":path")
+	if f.path == "/localreply_by_config" {
+		return f.sendLocalReplyInternal()
+	}
+	return api.Continue
+}
+
+/*
+The callbacks can be implemented on demand
+
+func (f *filter) DecodeData(buffer api.BufferInstance, endStream bool) api.StatusType {
+	return api.Continue
+}
+
+func (f *filter) DecodeTrailers(trailers api.RequestTrailerMap) api.StatusType {
+	return api.Continue
+}
+*/
+
+func (f *filter) EncodeHeaders(header api.ResponseHeaderMap, endStream bool) api.StatusType {
+	if f.path == "/update_upstream_response" {
+		header.Set("Content-Length", strconv.Itoa(len(UpdateUpstreamBody)))
+	}
+	header.Set("Rsp-Header-From-Go", "bar-test")
+	return api.Continue
+}
+
+// Callbacks which are called in response path
+func (f *filter) EncodeData(buffer api.BufferInstance, endStream bool) api.StatusType {
+	if f.path == "/update_upstream_response" {
+		if endStream {
+			buffer.SetString(UpdateUpstreamBody)
+		} else {
+			// TODO implement buffer->Drain, buffer.SetString means buffer->Drain(buffer.Len())
+			buffer.SetString("")
+		}
+	}
+	return api.Continue
+}
+
+/*
+The callbacks can be implemented on demand
+
+func (f *filter) EncodeTrailers(trailers api.ResponseTrailerMap) api.StatusType {
+	return api.Continue
+}
+
+func (f *filter) OnDestroy(reason api.DestroyReason) {
+}
+*/
+
+//var (
+//	// addr = flag.String("addr", "localhost:50051", "the address to connect to")
+//	addr = flag.String("addr", "prose-system.svc.cluster.local", "the address to connect to")
+//)
+//
+//func setup() {
+//	flag.Parse()
+//	// Set up a connection to the server.
+//	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+//	if err != nil {
+//		log.Fatalf("did not connect: %v", err)
+//	}
+//	defer conn.Close()
+//	c := pb.NewPrivacyProfileComposerClient(conn)
+//
+//	// Contact the server and print out its response.
+//	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+//	defer cancel()
+//
+//	_, err = c.PostObservedProfile(
+//		ctx,
+//		&pb.SvcObservedProfile{
+//			SvcInternalFQDN: "advertising.svc.internal",
+//			ObservedProcessingEntries: &pb.PurposeBasedProcessing{
+//				ProcessingEntries: nil},
+//		},
+//	)
+//	if err != nil {
+//		log.Fatalf("could not post observed profile: %v", err)
+//	}
+//
+//	profile, err := c.GetSystemWideProfile(ctx, &emptypb.Empty{})
+//	if err != nil {
+//		log.Fatalf("could not fetch system wide profile: %v", err)
+//	}
+//	log.Println(profile)
+//}
--- privacy-profile-composer/go.mod
+++ privacy-profile-composer/go.mod
@@ -1,16 +1,21 @@
 module privacy-profile-composer
 
-go 1.20
+go 1.18
 
 require (
+	github.com/cncf/xds/go v0.0.0-20230607035331-e9ce68804cb4
+	// Requires this commit or later: https://github.com/envoyproxy/envoy/commit/782bb0c102ffbd9880615526c54c00301a6f3f82
+	github.com/envoyproxy/envoy v1.27.1-0.20230915211138-782bb0c102ff
 	google.golang.org/grpc v1.58.0
 	google.golang.org/protobuf v1.31.0
 )
 
 require (
+	github.com/envoyproxy/protoc-gen-validate v1.0.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230711160842-782d3b101e98 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230711160842-782d3b101e98 // indirect
 )
