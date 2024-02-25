f4912bf19f42e75adfce5aed6297002db3b19c46
Miti Mazmudar <mitimazmudar@gmail.com>
2024-02-25 15:14:52 -0500

    inbound_filter.go: move piiAnalysis function to a file under the common package

--- privacy-profile-composer/pkg/envoyfilter/inbound_filter.go
+++ privacy-profile-composer/pkg/envoyfilter/inbound_filter.go
@@ -5,10 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
-	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"net/url"
 
 	"github.com/envoyproxy/envoy/contrib/golang/common/go/api"
@@ -129,56 +126,6 @@ func sendComposedProfile(fqdn string, purpose string, piiTypes []string, thirdPa
 	return api.Continue
 }
 
-func piiAnalysis(svcName string, bufferBytes []byte) (string, error) {
-	var jsonBody = `{
-			"key_F": {
-				"key_a1": "My phone number is 212-121-1424"
-			},
-			"URL": "www.abc.com",
-			"key_c": 3,
-			"names": ["James Bond", "Clark Kent", "Hakeem Olajuwon", "No name here!"],
-			"address": "123 Alpha Beta, Waterloo ON N2L3G1, Canada",
-			"DOB": "01-01-1989",
-			"gender": "Female",
-			"race": "Asian",
-			"language": "English"
-		}`
-
-	svcNameBuf, err := json.Marshal(svcName)
-	if err != nil {
-		return "", fmt.Errorf("could not marshal service name string into a valid JSON string: %w", err)
-	}
-
-	// TODO replace jsonBody with bufferBytes input arg
-	msgString := `{"json_to_analyze":` + jsonBody + `,"derive_purpose":` + string(svcNameBuf) + `}`
-
-	resp, err := http.Post("http://presidio.prose-system.svc.cluster.local:3000/batchanalyze", "application/json", bytes.NewBufferString(msgString))
-	if err != nil {
-		return "", fmt.Errorf("presidio post error: %w", err)
-	}
-
-	log.Printf("presidio responded '%v', content-length is %v bytes\n", resp.Status, resp.ContentLength)
-
-	jsonResp, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", fmt.Errorf("could not read Presidio response, %w", err)
-	}
-
-	err = resp.Body.Close()
-	if err != nil {
-		return "", fmt.Errorf("could not close presidio response body, %w", err)
-	}
-
-	log.Println("presidio response headers:")
-	for key, value := range resp.Header {
-		log.Printf("  \"%v\": %v\n", key, value)
-	}
-	log.Println("presidio response body:")
-	log.Printf("%v\n", string(jsonResp))
-
-	return string(jsonResp), nil
-}
-
 func (f *inboundFilter) DecodeData(buffer api.BufferInstance, endStream bool) api.StatusType {
 	log.Println(">>> DECODE DATA")
 	log.Println("  <<About to forward", buffer.Len(), "bytes of data to service>>")
@@ -213,7 +160,7 @@ func (f *inboundFilter) DecodeData(buffer api.BufferInstance, endStream bool) ap
 	}
 
 	var err error
-	if f.piiTypes, err = piiAnalysis(f.headerMetadata.SvcName, jsonBody); err != nil {
+	if f.piiTypes, err = common.PiiAnalysis(f.config.presidioUrl, f.headerMetadata.SvcName, jsonBody); err != nil {
 		log.Println(err)
 	}
 
