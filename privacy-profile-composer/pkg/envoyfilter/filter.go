f1bbaaa146b305dcc259b3635fad36db9ba45638
Nikita Volodin <volodin.n@gmail.com>
2024-03-13 15:39:29 -0400

    chore(prose/golang): extra logs
    - parsed envoy peer metadata
    - data passed to opa sdk

--- privacy-profile-composer/pkg/envoyfilter/filter.go
+++ privacy-profile-composer/pkg/envoyfilter/filter.go
@@ -343,6 +343,8 @@ func (f *Filter) runOPA(ctx context.Context, isDecode bool, dataItems []string) 
 
 	proseTags = map[string]string{}
 
+	log.Printf("direction: '%v'; isDecode: '%v', host: '%v', thirdPartyUrl: '%v'", f.config.direction, isDecode, f.headerMetadata.Host, f.thirdPartyURL)
+
 	// get the named policy decision for the specified input
 	result, err := f.opa.Decision(
 		ctx,
