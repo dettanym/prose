2a47162e16b136b71556171d9b7dbd35f945dfae
Nikita Volodin <volodin.n@gmail.com>
2024-03-21 14:19:20 -0400

    feat: scale up and down before latency test

--- docs/random-scripts/collect-latencies.sh
+++ docs/random-scripts/collect-latencies.sh
@@ -16,7 +16,34 @@ bookinfo_variants=(
   "filter"
 )
 
+echo "clean everything up before the test"
 for variant in "${bookinfo_variants[@]}"; do
+  ns=""
+  if [[ "${variant}" != "plain" ]]; then
+    ns="with-"
+  fi
+
+  kubectl scale --replicas 0 \
+    -n "bookinfo-${ns}${variant}"\
+    deployments --all >/dev/null
+done
+
+for variant in "${bookinfo_variants[@]}"; do
+  ns=""
+  if [[ "${variant}" != "plain" ]]; then
+    ns="with-"
+  fi
+
+  printf "Scaling up deployments for '%s' variant\n" "${variant}"
+  kubectl scale --replicas 10 \
+    -n "bookinfo-${ns}${variant}"\
+    deployments --all
+
+  printf "Waiting until ready\n"
+  kubectl wait --for condition=available --timeout 5m \
+    -n "bookinfo-${ns}${variant}" \
+    deployments --all
+
   printf "Testing '%s' variant\n" "${variant}"
   jq -ncM \
     --arg INGRESS_IP "${INGRESS_IP}" \
@@ -31,4 +58,9 @@ for variant in "${bookinfo_variants[@]}"; do
     | vegeta attack -format=json -insecure -duration=60s \
     | tee "${PRJ_ROOT}/evaluation/vegeta/bookinfo/${timestamp}_$(hostname)_${variant}.results.bin" \
     | vegeta report
+
+  printf "Scaling down deployments for '%s' variant\n" "${variant}"
+  kubectl scale --replicas 0 \
+    -n "bookinfo-${ns}${variant}"\
+    deployments --all
 done
