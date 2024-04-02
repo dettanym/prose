640f3249fe2e18580af9810703b0873407523811
Nikita Volodin <volodin.n@gmail.com>
2024-04-01 23:21:19 -0400

    feat(eval/latencies): run all rates and variants per test run
    Also rearranged folders with produced data. Also put everything into functions.

--- docs/random-scripts/collect-latencies.sh
+++ docs/random-scripts/collect-latencies.sh
@@ -13,139 +13,206 @@ rates=(
   "1000"
 )
 
-INGRESS_IP="192.168.49.21"
-
 bookinfo_variants=(
   "plain"
   "envoy"
   "filter"
 )
 
+INGRESS_IP="192.168.49.21"
+
+################################################################################
+
 PRJ_ROOT="$(/usr/bin/git rev-parse --show-toplevel)"
 hostname=$(hostname)
+timestamp=$(date -Iseconds)
 
-test_replicas=""
-case "${hostname}" in
-  click1|clack1|shiver)
-    test_replicas="10"
-  ;;
-  *)
-    test_replicas="2"
-  ;;
-esac
+function main() {
+  local test_results_dir="${PRJ_ROOT}/evaluation/vegeta/bookinfo/${hostname}/${timestamp}"
 
-echo "* suspend everything before the test"
-for variant in "${bookinfo_variants[@]}"; do
-  ns=""
-  if [[ "${variant}" != "plain" ]]; then
-    ns="with-"
-  fi
-
-  flux suspend kustomization "bookinfo-${ns}${variant}"
-  kubectl scale --replicas 0 \
-    -n "bookinfo-${ns}${variant}" \
-    deployments --all >/dev/null
-done
-
-run_tests () {
-  test_run_index="$1"
-
+  echo "* suspend everything before the test"
   for variant in "${bookinfo_variants[@]}"; do
-    ns=""
-    if [[ "${variant}" != "plain" ]]; then
-      ns="with-"
-    fi
-
-    printf "  - Scaling up deployments for '%s' variant\n" "${variant}"
-    kubectl scale --replicas "${test_replicas}" \
-      -n "bookinfo-${ns}${variant}" \
-      deployments --all >/dev/null
-
-    printf "  - Waiting until ready\n"
-    kubectl wait --for condition=available --timeout 5m \
-      -n "bookinfo-${ns}${variant}" \
-      deployments --all >/dev/null
-
-    printf "  - Testing '%s' variant\n" "${variant}"
-    jq -cM '.req' <"${test_results_dir}/${variant}.metadata.json" \
-      | vegeta attack -format=json -insecure "-duration=${DURATION}" "-rate=${RATE}" \
-      | vegeta encode --to json \
-      | zstd -c -T0 --ultra -20 - >"${test_results_dir}/${variant}_${test_run_index}.results.json.zst"
-
-    printf "  - report for '%s' variant\n" "${variant}"
-    zstd -c -d "${test_results_dir}/${variant}_${test_run_index}.results.json.zst" \
-      | vegeta report -type json \
-      | jq -M >"${test_results_dir}/${variant}_${test_run_index}.summary.json"
-
-    printf "  - Scaling down deployments for '%s' variant\n" "${variant}"
-    kubectl scale --replicas 0 \
-      -n "bookinfo-${ns}${variant}" \
-      deployments --all >/dev/null
+    alter_fluxtomizations "suspend" "$variant"
   done
-}
 
-# TODO: this rate loop should be inner most loop:
-#  inside run_tests and inside bookinfo_variants
-for RATE in "${rates[@]}"; do
-  timestamp=$(date -Iseconds)
-  test_results_dir="${PRJ_ROOT}/evaluation/vegeta/bookinfo/${hostname}/${timestamp}"
-  mkdir -p "${test_results_dir}"
-
-  echo "* create metadata files"
-  for variant in "${bookinfo_variants[@]}"; do
-    ns=""
-    if [[ "${variant}" != "plain" ]]; then
-      ns="with-"
-    fi
-
-    jq -nM \
-      --arg timestamp "${timestamp}" \
-      --arg INGRESS_IP "${INGRESS_IP}" \
-      --arg variant "${variant}" \
-      --arg duration "${DURATION}" \
-      --arg rate "${RATE}" \
-      --arg hostname "${hostname}" \
-      --arg test_replicas "${test_replicas}" \
-      --arg ns "${ns}" \
-      '{
-        timestamp: $timestamp,
-        resultsFileSuffix: ".results.json.zst",
-        summaryFileSuffix: ".summary.json",
-        req: {
-          method: "GET",
-          url: ("https://" + $INGRESS_IP + "/productpage?u=test"),
-          header: {
-            Host: ["bookinfo-" + $variant + ".my-example.com"],
-          },
-        },
-        testOptions: {
-          duration: $duration,
-          rate: $rate,
-        },
-        workloadInfo: {
-          variant: $variant,
-          namespace: ("bookinfo-" + $ns + $variant),
-          hostname: $hostname,
-          test_replicas: $test_replicas,
-        },
-      }' >"${test_results_dir}/${variant}.metadata.json"
+  echo "* writing all metadata files"
+  for RATE in "${rates[@]}"; do
+    for VARIANT in "${bookinfo_variants[@]}"; do
+      write_metadata_file \
+        "${test_results_dir}/${RATE}/${VARIANT}" \
+        "$RATE" "$VARIANT"
+    done
   done
 
   for i in $(seq 1 100); do
-    printf "* run test '%s'\n" "${i}"
-    run_tests "$i"
+    for RATE in "${rates[@]}"; do
+      for VARIANT in "${bookinfo_variants[@]}"; do
+        printf "* Running test #%s against variant '%s' with rate '%s'\n" "$i" "$VARIANT" "$RATE"
+        run_test "$i" "${test_results_dir}/${RATE}/${VARIANT}"
+      done
+    done
   done
-done
 
-echo "* resume everything after the test"
-for variant in "${bookinfo_variants[@]}"; do
-  ns=""
+  echo "* resume everything after the test"
+  for variant in "${bookinfo_variants[@]}"; do
+    alter_fluxtomizations "resume" "$variant"
+  done
+
+  completion_time=$(date -Iseconds)
+  echo "* Completed at ${completion_time}"
+
+  printf "%s\n" "$completion_time" > "${test_results_dir}/completion_time.txt"
+}
+
+function write_metadata_file() {
+  local dir="$1"
+
+  local rate="$2"
+  local variant="$3"
+
+  mkdir -p "$dir"
+  jq -nM \
+    --arg timestamp "${timestamp}" \
+    --arg INGRESS_IP "${INGRESS_IP}" \
+    --arg variant "${variant}" \
+    --arg duration "${DURATION}" \
+    --arg rate "${rate}" \
+    --arg hostname "${hostname}" \
+    --arg test_replicas "$(get_test_replicas)" \
+    --arg ns "$(get_resource_name "$variant")" \
+    '{
+      timestamp: $timestamp,
+      resultsFileSuffix: ".results.json.zst",
+      summaryFileSuffix: ".summary.json",
+      req: {
+        method: "GET",
+        url: ("https://" + $INGRESS_IP + "/productpage?u=test"),
+        header: {
+          Host: ["bookinfo-" + $variant + ".my-example.com"],
+        },
+      },
+      testOptions: {
+        duration: $duration,
+        rate: $rate,
+      },
+      workloadInfo: {
+        variant: $variant,
+        namespace: $ns,
+        hostname: $hostname,
+        test_replicas: $test_replicas,
+      },
+    }' >"${dir}/metadata.json"
+}
+
+function run_test() {
+  local test_run_index="$1"
+  local test_results_dir="$2"
+
+  local metadata
+  metadata=$(cat "$test_results_dir/metadata.json")
+
+  local variant
+  local name
+  local test_replicas
+  local results_file_suffix
+  local summary_file_suffix
+  local duration
+  local rate
+  local req
+
+  variant="$(echo "$metadata" | jq -rM '.workloadInfo.variant')"
+  name="$(echo "$metadata" | jq -rM '.workloadInfo.namespace')"
+  test_replicas="$(echo "$metadata" | jq -rM '.workloadInfo.test_replicas')"
+  results_file_suffix="$(echo "$metadata" | jq -rM '.resultsFileSuffix')"
+  summary_file_suffix="$(echo "$metadata" | jq -rM '.summaryFileSuffix')"
+  duration="$(echo "$metadata" | jq -rM '.testOptions.duration')"
+  rate="$(echo "$metadata" | jq -rM '.testOptions.rate')"
+  req="$(echo "$metadata" | jq -cM '.req')"
+
+  local results_file="${test_results_dir}/${test_run_index}${results_file_suffix}"
+  local summary_file="${test_results_dir}/${test_run_index}${summary_file_suffix}"
+
+  printf "  - Scaling up deployments for '%s' variant\n" "${variant}"
+  scale_deployments "${name}" "${test_replicas}"
+  printf "  - Waiting until ready\n"
+  wait_until_ready "${name}"
+
+  printf "  - Testing '%s' variant\n" "${variant}"
+  echo "$req" \
+    | vegeta attack -format=json -insecure "-duration=${duration}" "-rate=${rate}" \
+    | vegeta encode --to json \
+    | zstd -c -T0 --ultra -20 - >"${results_file}"
+
+  printf "  - report for '%s' variant\n" "${variant}"
+  zstd -c -d "${results_file}" \
+    | vegeta report -type json \
+    | jq -M >"${summary_file}"
+
+  printf "  - Scaling down deployments for '%s' variant\n" "${variant}"
+  scale_deployments "${name}" "0"
+}
+
+function get_test_replicas() {
+  local test_replicas=""
+
+  case "${hostname}" in
+    click1|clack1|shiver)
+      test_replicas="10"
+    ;;
+    *)
+      test_replicas="2"
+    ;;
+  esac
+
+  printf "%s" "$test_replicas"
+}
+
+function get_resource_name() {
+  local variant="$1"
+
+  local ns=""
   if [[ "${variant}" != "plain" ]]; then
     ns="with-"
   fi
 
-  kubectl scale --replicas 1 \
-    -n "bookinfo-${ns}${variant}" \
+  printf "bookinfo-%s%s" "${ns}" "${variant}"
+}
+
+function scale_deployments() {
+  local name="$1"
+  local number="$2"
+
+  kubectl scale --replicas "${number}" \
+    -n "${name}" \
     deployments --all >/dev/null
-  flux resume kustomization --wait=false "bookinfo-${ns}${variant}"
-done
+}
+
+function wait_until_ready() {
+  local name="$1"
+
+  kubectl wait --for condition=available --timeout 5m \
+    -n "$name" \
+    deployments --all >/dev/null
+}
+
+function alter_fluxtomizations() {
+  local action="$1"
+  local variant="$2"
+  local ns
+
+  ns="$(get_resource_name "$variant")"
+
+  case "$action" in
+    suspend)
+      flux suspend kustomization "$ns"
+      scale_deployments "$ns" 0
+      ;;
+    resume)
+      scale_deployments "$ns" 1
+      flux resume kustomization --wait=false "$ns"
+      ;;
+    *) ;;
+  esac
+}
+
+main
