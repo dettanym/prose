c72a82fa86f16e69df77203fc7da427497ed603b
Nikita Volodin <volodin.n@gmail.com>
2024-04-11 17:34:47 -0400

    feat(eval): split minikube command from create step

--- evaluation/scripts/start-minikube-cluster.sh
+++ evaluation/scripts/start-minikube-cluster.sh
@@ -1,43 +1,44 @@
 #!/usr/bin/env -S bash -c '"$(dirname $(readlink -f "$0"))/../env.sh" bash "$0" "$@"'
 # shellcheck disable=SC2096
 
+set -euo pipefail
+
 ENABLE_METALLB="false"
 
 PRJ_ROOT="$(git rev-parse --show-toplevel)"
+MINIKUBE="$(dirname "$(readlink -f "$0")")/minikube.sh"
 
 minikube_start=(
-  "minikube"
   "start"
   "--driver=docker"
-  "--subnet='192.168.49.0/24'"
+  "--subnet=192.168.49.0/24"
 )
-
-ripple_settings() {
-  export MINIKUBE_HOME="/usr/local/home/$USER/.minikube"
-}
 
 case "$(hostname)" in
 click1 | clack1)
-  ripple_settings
-  echo "creating minikube in ripple"
-  "${minikube_start[@]}" \
-    --nodes=5 --cpus=12 --memory=90g
+  echo "minikube in ripple"
+  minikube_start+=(
+    "--nodes=5" "--cpus=12" "--memory=90g"
+  )
   ;;
 shiver)
-  ripple_settings
-  echo "creating minikube in ripple"
-  "${minikube_start[@]}" \
-    --nodes=3 --cpus=10 --memory=100g
+  echo "minikube in ripple"
+  minikube_start+=(
+    "--nodes=3" "--cpus=10" "--memory=100g"
+  )
   ;;
 *)
-  echo "creating minikube outside of ripple"
-  "${minikube_start[@]}" \
-    --nodes=1 --cpus=4 --memory=8G
+  echo "minikube outside of ripple"
+  minikube_start+=(
+    "--nodes=1" "--cpus=4" "--memory=8G"
+  )
   ;;
 esac
 
+"$MINIKUBE" "${minikube_start[@]}"
+
 if [[ ${ENABLE_METALLB} == "true" ]]; then
-  minikube addons enable metallb
+  "$MINIKUBE" addons enable metallb
   cat <<-EOF | kubectl apply -f-
 		apiVersion: v1
 		kind: ConfigMap
