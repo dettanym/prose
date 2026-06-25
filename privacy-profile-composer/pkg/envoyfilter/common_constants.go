68efe62f8b090bbf1364600f952228d31d2e1326
igen_aru <111181527+igen-aru@users.noreply.github.com>
2026-06-24 21:34:03 -0600

    Update common_constants.go

--- privacy-profile-composer/pkg/envoyfilter/common_constants.go
+++ privacy-profile-composer/pkg/envoyfilter/common_constants.go
@@ -4,8 +4,8 @@ package envoyfilter
 //  So these constants are shared between the data and control planes.
 
 const (
-	PROSE_SIDECAR_DIRECTION = "PROSE_SIDECAR_DIRECTION"
-	PROSE_DATA_FLOW         = "PROSE_DATA_FLOW"
+	PROSE_SIDECAR_DIRECTION = "prose_sidecar_direction"
+	PROSE_DATA_FLOW         = "prose_data_flow"
 	PROSE_PII_TYPES         = "prose_pii_types"
 	PROSE_OPA_ENFORCE       = "prose_opa_enforce"
 	PROSE_OPA_DECISION      = "prose_opa_decision"
