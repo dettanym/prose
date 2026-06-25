49bdd53f341f150fd5a0964fd922d8068a817ddf
igen_aru <111181527+igen-aru@users.noreply.github.com>
2026-06-25 17:45:20 -0600

    Update common_constants.go

--- privacy-profile-composer/pkg/envoyfilter/common_constants.go
+++ privacy-profile-composer/pkg/envoyfilter/common_constants.go
@@ -1,18 +1,23 @@
 package envoyfilter
 
+// TODO: The tracing unit should use these as well.
+//  So these constants are shared between the data and control planes.
 
-const PROSE_SIDECAR_DIRECTION = "prose_sidecar_direction"
-const PROSE_DATA_FLOW = "prose_data_flow"
-const PROSE_PII_TYPES = "prose_pii_types"
-const PROSE_PII_COMPLIANT = "prose_pii_compliant"
-const PROSE_PII_VIOLATION = "prose_pii_violation"
-const PROSE_OPA_ENFORCE = "prose_opa_enforce"
-const PROSE_OPA_DECISION = "prose_opa_decision"
-const PROSE_VIOLATION_TYPE = "prose_violation_type"
-const PROSE_EXTERNAL_DOMAIN = "prose_external_domain"
-const PROSE_PRESIDIO_ERROR = "prose_presidio_error"
-const PROSE_JSON_BODY_ERROR = "prose_json_body_error"
-const PROSE_OPA_ERROR = "prose_opa_error"
-const DataSharing = "DATA_SHARING"
-const PurposeOfUseDirect = "PURPOSE_OF_USE_DIRECT"
-const PurposeOfUseIndirect = "PURPOSE_OF_USE_INDIRECT"
+const (
+	PROSE_SIDECAR_DIRECTION = "prose_sidecar_direction"
+	PROSE_DATA_FLOW         = "prose_data_flow"
+	PROSE_PII_TYPES         = "prose_pii_types"
+	PROSE_OPA_ENFORCE       = "prose_opa_enforce"
+	PROSE_OPA_DECISION      = "prose_opa_decision"
+	PROSE_VIOLATION_TYPE    = "prose_violation_type"
+	PROSE_EXTERNAL_DOMAIN   = "prose_external_domain"
+	PROSE_PRESIDIO_ERROR    = "prose_presidio_error"
+	PROSE_JSON_BODY_ERROR   = "prose_json_body_error"
+	PROSE_OPA_ERROR         = "prose_opa_error"
+)
+
+const (
+	DataSharing          string = "DATA_SHARING"
+	PurposeOfUseDirect          = "PURPOSE_OF_USE_DIRECT"
+	PurposeOfUseIndirect        = "PURPOSE_OF_USE_INDIRECT"
+)
